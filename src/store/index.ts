95cdf7df98e87beb61a69986dfe6935fb3688b53
xuqingkai <xuqingkai@hd123.com>
2023-08-16 22:05:02 +0800

    docs: ✏️  演示小程序暗黑模式配置调整

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,12 +4,22 @@ const isDark = ref<boolean>(false)
 
 function setDark(dark: boolean) {
   isDark.value = dark
+  // #ifdef H5
   process.env.NODE_ENV === 'development' && uni.setStorageSync('isDark', dark)
+  // #endif
+  // #ifndef H5
+  uni.setStorageSync('isDark', dark)
+  // #endif
 }
 
 export function useDark() {
+  // #ifdef H5
   process.env.NODE_ENV === 'development'
     ? setDark(Boolean(uni.getStorageSync('isDark')))
     : setDark(localStorage.getItem('vitepress-theme-appearance') === 'dark')
+  // #endif
+  // #ifndef H5
+  setDark(Boolean(uni.getStorageSync('isDark')))
+  // #endif
   return { isDark, setDark }
 }
