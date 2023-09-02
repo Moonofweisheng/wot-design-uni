819bbbca6ab1c999096936da5e9d2dd664e480ce
xuqingkai <xuqingkai@hd123.com>
2023-09-02 23:23:33 +0800

    fix: 🐛 修复Grid组件不展示border的问题

--- src/uni_modules/wot-design-uni/components/wd-grid-item/wd-grid-item.vue
+++ src/uni_modules/wot-design-uni/components/wd-grid-item/wd-grid-item.vue
@@ -29,7 +29,7 @@ export default {
 </script>
 
 <script lang="ts" setup>
-import { getCurrentInstance, inject, onBeforeMount, onMounted, ref, nextTick, computed, Ref, watch } from 'vue'
+import { getCurrentInstance, inject, onBeforeMount, onMounted, ref, Ref, watch } from 'vue'
 
 type BadgeType = 'primary' | 'success' | 'warning' | 'danger' | 'info'
 type LinkType = 'navigateTo' | 'switchTab' | 'reLaunch' | 'redirectTo'
@@ -126,7 +126,7 @@ function init() {
   style.value = `width: ${width}; ${squareStyle || gutterStyle}`
 }
 
-function click(event) {
+function click() {
   if (!parent.clickable) return
   const { url, linkType } = props
   if (url) {
