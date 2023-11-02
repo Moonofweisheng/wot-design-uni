78153facc36c2780563896422ad8e227ae48af8c
xuqingkai <xuqingkai@hd123.com>
2023-11-02 19:20:22 +0800

    feat: ✨ 增加 Sidebar 组件文件

---
+++ src/uni_modules/wot-design-uni/components/wd-sidebar-item/wd-sidebar-item.vue
@@ -0,0 +1,38 @@
+<template>
+  <view :class="`wd-sidebar-item ${customClass}`">
+    <wd-badge></wd-badge>
+  </view>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'wd-sidebar-item',
+  options: {
+    addGlobalClass: true,
+    virtualHost: true,
+    styleIsolation: 'shared'
+  }
+}
+</script>
+
+<script lang="ts" setup>
+interface Props {
+  modelValue?: number | string
+  // 自定义样式
+  customStyle?: string
+  // 自定义样式类
+  customClass?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: 0,
+  customStyle: '',
+  customClass: '',
+  badgeProps: () => {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
---
+++ src/uni_modules/wot-design-uni/components/wd-sidebar/wd-sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+  <view :class="`wd-sidebar ${customClass}`"></view>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'wd-sidebar',
+  options: {
+    addGlobalClass: true,
+    virtualHost: true,
+    styleIsolation: 'shared'
+  }
+}
+</script>
+
+<script lang="ts" setup>
+interface Props {
+  modelValue?: number | string
+  // 自定义样式
+  customStyle?: string
+  // 自定义样式类
+  customClass?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: 0,
+  customStyle: '',
+  customClass: ''
+})
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
