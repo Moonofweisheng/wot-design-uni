28519689762b5c21ef421ab45554805116251daa
xuqingkai <xuqingkai@hd123.com>
2023-11-06 22:27:41 +0800

    feat: ✨ 增加fab组件

---
+++ src/uni_modules/wot-design-uni/components/wd-fab/wd-fab.vue
@@ -0,0 +1,32 @@
+<template>
+  <view :class="customClass" :style="customStyle"></view>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'wd-fab',
+  options: {
+    virtualHost: true,
+    addGlobalClass: true,
+    styleIsolation: 'shared'
+  }
+}
+</script>
+
+<script lang="ts" setup>
+import { computed, onBeforeMount, ref, watch } from 'vue'
+
+interface Props {
+  customClass?: string
+  customStyle?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  customClass: '',
+  customStyle: ''
+})
+</script>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
