f68868b0c6f91f687d97dc903ff77125aab4ad10
weisheng <1780903673@qq.com>
2023-11-29 11:10:15 +0800

    Revert "feat: ✨ 重构数字键盘"
    This reverts commit ca099be2c90b4ac65d72053409468d7dc59dfdbf.

--- src/uni_modules/wot-design-uni/components/wd-number-keyboard/wd-number-keyboard.vue
+++ src/uni_modules/wot-design-uni/components/wd-number-keyboard/wd-number-keyboard.vue
@@ -7,12 +7,12 @@
     :modal-style="modal ? '' : 'opacity: 0;'"
     :modal="hideOnClickOutside"
     :lockScroll="lockScroll"
-    @click-modal="handleClose"
+    @click-modal="onClose"
   >
     <view :class="`wd-number-keyboard ${customClass}`" :style="customStyle">
       <view class="wd-number-keyboard__header" v-if="showTitle">
         <text class="wd-number-keyboard__title">{{ title }}</text>
-        <view class="wd-number-keyboard__close" hover-class="wd-number-keyboard__close--hover" v-if="showClose" @click="handleClose">
+        <view class="wd-number-keyboard__close" hover-class="wd-number-keyboard__close--hover" v-if="showClose" @click="onClose">
           <text>{{ closeText }}</text>
         </view>
       </view>
@@ -138,65 +138,69 @@ const emit = defineEmits(['update:visible', 'input', 'close', 'delete', 'update:
  * @returns 打乱顺序后的数组
  */
 function shuffleArray<T>(arr: T[]): T[] {
-  const newArr = [...arr]
-  for (let i = newArr.length - 1; i > 0; i--) {
+  for (let i = arr.length - 1; i > 0; i--) {
     // 生成一个随机索引 j，范围是 [0, i]
     const j = Math.floor(Math.random() * (i + 1))
 
     // 交换索引 i 和 j 处的元素
-    ;[newArr[i], newArr[j]] = [newArr[j], newArr[i]]
+    ;[arr[i], arr[j]] = [arr[j], arr[i]]
   }
-  return newArr
-}
-function genBasicKeys(): Key[] {
-  const keys = Array.from({ length: 9 }, (_, i) => ({ text: i + 1 }))
-
-  // 如果需要随机顺序，则调用 shuffleArray 方法打乱数组顺序
-  return props.randomKeyOrder ? shuffleArray(keys) : keys
+  return arr
 }
 
-function genDefaultKeys(): Key[] {
-  return [
-    ...genBasicKeys(),
-    { text: props.extraKey as string, type: 'extra' },
-    { text: 0 },
-    {
-      // 根据条件是否显示删除键的文本和类型
-      text: props.showDeleteKey ? props.deleteText : '',
-      type: props.showDeleteKey ? 'delete' : ''
-    }
-  ]
+const genBasicKeys = () => {
+  let keys: Key[] = Array(9)
+    .fill('')
+    .map((_, i) => ({ text: i + 1 }))
+
+  if (props.randomKeyOrder) {
+    keys = shuffleArray(keys)
+  }
+
+  return keys
 }
 
-function genCustomKeys(): Key[] {
+const genDefaultKeys = (): Key[] => [
+  ...genBasicKeys(),
+  { text: props.extraKey as string, type: 'extra' },
+  { text: 0 },
+  {
+    text: props.showDeleteKey ? props.deleteText : '',
+    type: props.showDeleteKey ? 'delete' : ''
+  }
+]
+
+const genCustomKeys = () => {
   const keys = genBasicKeys()
-  const extraKeys = Array.isArray(props.extraKey) ? props.extraKey : [props.extraKey]
+  const { extraKey } = props
+  const extraKeys = Array.isArray(extraKey) ? extraKey : [extraKey]
+
   if (extraKeys.length === 1) {
-    // 如果只有一个额外按键，则添加一个宽度较大的数字0和一个额外按键
     keys.push({ text: 0, wider: true }, { text: extraKeys[0], type: 'extra' })
   } else if (extraKeys.length === 2) {
-    // 如果有两个额外按键，则添加两个额外按键和一个数字0
     keys.push({ text: extraKeys[0], type: 'extra' }, { text: 0 }, { text: extraKeys[1], type: 'extra' })
   }
 
   return keys
 }
 
-const handleClose = () => {
+const onClose = () => {
   emit('close')
   emit('update:visible', false)
 }
 
 const handlePress = (text: string, type: KeyType) => {
   if (text === '' && type === 'extra') {
-    return handleClose()
+    return onClose()
   }
+
   const value = props.modelValue
+
   if (type === 'delete') {
     emit('delete')
     emit('update:modelValue', value.slice(0, value.length - 1))
   } else if (type === 'close') {
-    handleClose()
+    onClose()
   } else if (value.length < +props.maxlength) {
     emit('input', text)
     emit('update:modelValue', value + text)
