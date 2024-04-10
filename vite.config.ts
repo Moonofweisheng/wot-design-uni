eaa6926767aa48d90d58117c0688b9df2de6e112
weisheng <1780903673@qq.com>
2024-04-10 09:49:09 +0800

    docs: ✏️  优化文档及演示demo访问速度

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
 /*
  * @Author: weisheng
  * @Date: 2023-03-21 22:49:24
- * @LastEditTime: 2023-08-14 23:07:46
+ * @LastEditTime: 2024-04-09 23:03:26
  * @LastEditors: weisheng
  * @Description:
- * @FilePath: \wot-design-uni\vite.config.ts
+ * @FilePath: /wot-design-uni/vite.config.ts
  * 记得注释
  */
 import { defineConfig } from 'vite'
+// #ifdef H5
+import viteCompression from 'vite-plugin-compression'
+// #endif
+
 import uni from '@dcloudio/vite-plugin-uni'
 
 export default defineConfig({
   base: './',
-  plugins: [uni()]
+  plugins: [
+    uni(),
+    // #ifdef H5
+    viteCompression()
+    // #endif
+  ]
 })
