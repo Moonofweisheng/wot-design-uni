19127b8f27c43be90f50168682275c0648646e2a
mofeng <wanyang@xiaohongshu.com>
2024-05-08 10:21:39 +0800

    fix: 修复wd-calendar未抛出事件

--- src/uni_modules/wot-design-uni/components/wd-calendar/wd-calendar.vue
+++ src/uni_modules/wot-design-uni/components/wd-calendar/wd-calendar.vue
@@ -123,7 +123,7 @@ import { useCell } from '../composables/useCell'
 import { FORM_KEY, type FormItemRule } from '../wd-form/types'
 import { useParent } from '../composables/useParent'
 import { useTranslate } from '../composables/useTranslate'
-import { calendarProps } from './types'
+import { calendarProps, CalendarExpose } from './types'
 import type { CalendarType } from '../wd-calendar-view/types'
 const { translate } = useTranslate('calendar')
 
@@ -409,6 +409,11 @@ function handleShortcutClick(index: number) {
     handleConfirm()
   }
 }
+
+defineExpose<CalendarExpose>({
+  close,
+  open
+})
 </script>
 
 <style lang="scss" scoped>
