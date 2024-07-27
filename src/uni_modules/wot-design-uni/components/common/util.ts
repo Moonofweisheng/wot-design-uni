3d7775c5a93668cb7ac6b50563869d13bbd39bfa
weisheng <1780903673@qq.com>
2024-07-27 15:16:21 +0800

    fix: 🐛 修复addUnit工具方法为string类型的参数时未添加单位导致swiper高度丢失的问题

--- src/uni_modules/wot-design-uni/components/common/util.ts
+++ src/uni_modules/wot-design-uni/components/common/util.ts
@@ -22,7 +22,7 @@ function s4() {
  * @return {string} num+px
  */
 export function addUnit(num: number | string) {
-  return !isNumber(num) ? num : `${num}px`
+  return Number.isNaN(Number(num)) ? `${num}` : `${num}px`
 }
 
 /**
