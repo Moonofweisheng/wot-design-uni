d37d19795765eb4b57bd9b6b5aca534483a1ac68
chenjianou <chenjianou98@gmail.com>
2024-08-12 13:18:08 +0800

    feat: 添加了禁用input时可触发点击事件

--- src/uni_modules/wot-design-uni/components/wd-input-number/wd-input-number.vue
+++ src/uni_modules/wot-design-uni/components/wd-input-number/wd-input-number.vue
@@ -5,16 +5,20 @@
     </view>
     <view v-if="!withoutInput" class="wd-input-number__inner" @click.stop="">
       <input
+        v-if="!disableInput"
         class="wd-input-number__input"
         :style="`${inputWidth ? 'width: ' + inputWidth : ''}`"
         type="digit"
-        :disabled="disabled || disableInput"
+        :disabled="disabled"
         v-model="inputValue"
         :placeholder="placeholder"
         @input="handleInput"
         @focus="handleFocus"
         @blur="handleBlur"
       />
+      <view @click="handleClickInput" v-else :style="`${inputWidth ? 'width: ' + inputWidth : ''}`" class="wd-input-number__input">
+        {{ inputValue }}
+      </view>
       <view class="wd-input-number__input-border"></view>
     </view>
     <view :class="`wd-input-number__action ${maxDisabled || disablePlus ? 'is-disabled' : ''}`" @click="add">
@@ -40,7 +44,7 @@ import { debounce, isDef, isEqual } from '../common/util'
 import { inputNumberProps } from './types'
 
 const props = defineProps(inputNumberProps)
-const emit = defineEmits(['focus', 'blur', 'change', 'update:modelValue'])
+const emit = defineEmits(['focus', 'blur', 'change', 'update:modelValue', 'clickInput'])
 
 const minDisabled = ref<boolean>(false)
 const maxDisabled = ref<boolean>(false)
@@ -160,7 +164,9 @@ function handleInput(event: any) {
 function handleFocus(event: any) {
   emit('focus', event.detail)
 }
-
+function handleClickInput(event: any) {
+  emit('clickInput')
+}
 function handleBlur() {
   const value = formatValue(inputValue.value)
   if (!isEqual(inputValue.value, value)) {
