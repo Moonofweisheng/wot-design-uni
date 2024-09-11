194712b88c9aecf0b22b464ab4fe111c2d09f275
Joker <avboy520@gmail.com>
2024-09-11 14:00:34 +0800

    fix: wd-loadmore组件props 属性errorText默认值导致国际化失效

--- src/uni_modules/wot-design-uni/components/wd-loadmore/wd-loadmore.vue
+++ src/uni_modules/wot-design-uni/components/wd-loadmore/wd-loadmore.vue
@@ -2,14 +2,9 @@
   <view :class="['wd-loadmore', customClass]" :style="customStyle" @click="reload">
     <wd-divider v-if="state === 'finished'">{{ finishedText || translate('finished') }}</wd-divider>
     <block v-if="state === 'error'">
-      <block v-if="errorText">
-        {{ errorText }}
-      </block>
-      <block v-else>
-        <text class="wd-loadmore__text">{{ translate('error') }}</text>
-        <text class="wd-loadmore__text is-light">{{ translate('retry') }}</text>
-        <wd-icon name="refresh" custom-class="wd-loadmore__refresh" />
-      </block>
+      <text class="wd-loadmore__text">{{ errorText || translate('error') }} </text>
+      <text class="wd-loadmore__text is-light">{{ translate('retry') }}</text>
+      <wd-icon name="refresh" custom-class="wd-loadmore__refresh" />
     </block>
     <block v-if="state === 'loading'">
       <wd-loading custom-class="wd-loadmore__loading" />
