0c9560936d93bd0b1f555c2d125fa1c01ed48857
wangxq <wangxq0614@foxmail.com>
2024-09-16 16:39:50 +0800

    fix: 🐛 修复小程序端演示案例错乱的问题。不能将id直接绑定在自定义组件上面啊！！！

--- src/pages/tour/Index.vue
+++ src/pages/tour/Index.vue
@@ -2,15 +2,19 @@
   <page-wraper>
     <view>
       <demo-block title="基本用法">
-        <wd-cell title="步骤一" id="pop1001">
-          <wd-button @click="show1 = true">点击试试</wd-button>
+        <wd-cell title="步骤一">
+          <view id="pop1001">
+            <wd-button @click="show1 = true">点击试试</wd-button>
+          </view>
         </wd-cell>
       </demo-block>
       <wd-tour v-model="show1" :steps="steps1" type="card" />
 
       <demo-block title="自定义样式">
-        <wd-cell title="步骤一" id="pop2001">
-          <wd-button @click="show2 = true">点击试试</wd-button>
+        <wd-cell title="步骤一">
+          <view id="pop2001">
+            <wd-button @click="show2 = true">点击试试</wd-button>
+          </view>
         </wd-cell>
       </demo-block>
       <wd-tour v-model="show2" :steps="steps2" type="card" bgColor="#f00">
@@ -32,15 +36,6 @@
         </view>
       </demo-block>
       <wd-tour v-model="show3" :steps="steps3" type="step" />
-
-      <view @click="show4 = true">
-        <wd-tabbar fixed bordered safeAreaInsetBottom placeholder>
-          <wd-tabbar-item id="pop4001" title="首页" icon="home"></wd-tabbar-item>
-          <wd-tabbar-item id="pop4002" title="分类" icon="cart"></wd-tabbar-item>
-          <wd-tabbar-item id="pop4003" title="我的" icon="user"></wd-tabbar-item>
-        </wd-tabbar>
-      </view>
-      <wd-tour v-model="show4" :steps="steps4" type="step" />
     </view>
   </page-wraper>
 </template>
@@ -50,7 +45,6 @@ import { ref } from 'vue'
 const show1 = ref<boolean>(false)
 const show2 = ref<boolean>(false)
 const show3 = ref<boolean>(false)
-const show4 = ref<boolean>(false)
 
 const steps1 = [
   {
@@ -81,20 +75,6 @@ const steps3 = [
   {
     content: '引导步骤2',
     target: 'pop3002'
-  }
-]
-const steps4 = [
-  {
-    content: '首页',
-    target: 'pop4001'
-  },
-  {
-    content: '分类',
-    target: 'pop4002'
-  },
-  {
-    content: '我的',
-    target: 'pop4003'
   }
 ]
 </script>
