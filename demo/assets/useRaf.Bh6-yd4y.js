6ba4ecc9257f6993321066ef546dc4096713cdd3
Moonofweisheng <1780903673@qq.com>
2024-10-12 05:00:25 +0000

    Deploying to gh-pages from @ Moonofweisheng/wot-design-uni@60b07e514b836c55305536e2ba29ec5239b62de3 🚀

---
+++ demo/assets/useRaf.Bh6-yd4y.js
@@ -0,0 +1,1 @@
+import{r as a,O as e}from"./index-B7o-V5at.js";import{I as r,t}from"./page-wraper.CdpHa-_z.js";function n(n){const o=a(null),s=()=>{r(o.value)?cancelAnimationFrame(o.value):t(o.value)&&clearTimeout(o.value)};return e((()=>{s()})),{start:()=>{const a=a=>{n(a)};o.value=requestAnimationFrame(a)},cancel:s}}export{n as u};
