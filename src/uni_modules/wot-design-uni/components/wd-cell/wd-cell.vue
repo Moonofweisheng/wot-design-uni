5aa03f674465ee20988ce191180d1e172c0ce190
Jasper <wx2080969361@gmail.com>
2024-10-16 15:45:54 +0800

    refactor: ♻️  替换`??`运算符为更兼容的方式

--- src/uni_modules/wot-design-uni/components/wd-cell/wd-cell.vue
+++ src/uni_modules/wot-design-uni/components/wd-cell/wd-cell.vue
@@ -63,6 +63,7 @@ import { useCell } from '../composables/useCell'
 import { useParent } from '../composables/useParent'
 import { FORM_KEY } from '../wd-form/types'
 import { cellProps } from './types'
+import { isNone } from '../common/util'
 
 const props = defineProps(cellProps)
 const emit = defineEmits(['click'])
@@ -70,7 +71,7 @@ const emit = defineEmits(['click'])
 const cell = useCell()
 
 const isBorder = computed(() => {
-  return Boolean(props.border ?? cell.border.value)
+  return Boolean(!isNone(props.border) ? props.border : cell.border.value)
 })
 
 const { parent: form } = useParent(FORM_KEY)
