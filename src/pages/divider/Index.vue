c25636499bb6393a14ab70dedce995681db2b494
Moonofweisheng <1780903673@qq.com>
2024-11-26 10:51:11 +0800

    feat: ✨ 优化 Divider 分割线功能支持虚线、垂直等功能

--- src/pages/divider/Index.vue
+++ src/pages/divider/Index.vue
@@ -1,21 +1,51 @@
-<!--
- * @Author: weisheng
- * @Date: 2023-06-13 11:47:12
- * @LastEditTime: 2023-08-07 20:19:49
- * @LastEditors: weisheng
- * @Description: 
- * @FilePath: \wot-design-uni\src\pages\divider\Index.vue
- * 记得注释
--->
 <template>
   <page-wraper>
     <demo-block title="基本用法" transparent>
-      <wd-divider>这是分割线</wd-divider>
+      <wd-divider></wd-divider>
     </demo-block>
+
+    <demo-block title="展示文本" transparent>
+      <wd-divider>展示文本</wd-divider>
+    </demo-block>
+
+    <demo-block title="自定义渲染内容" transparent>
+      <wd-divider>
+        <wd-icon name="arrow-down" size="20" />
+      </wd-divider>
+    </demo-block>
+
+    <demo-block title="内容位置" transparent>
+      <wd-divider>中间</wd-divider>
+      <wd-divider content-position="left">左侧</wd-divider>
+      <wd-divider content-position="right">右侧</wd-divider>
+    </demo-block>
+
+    <demo-block title="虚线" transparent>
+      <wd-divider dashed>虚线分割</wd-divider>
+    </demo-block>
+
     <demo-block title="自定义颜色" transparent>
       <wd-divider color="#4D80F0">自定义颜色</wd-divider>
+    </demo-block>
+
+    <demo-block title="垂直分割线" transparent>
+      <view class="content">
+        文本
+        <wd-divider vertical />
+        文本
+        <wd-divider vertical dashed />
+        文本
+        <wd-divider vertical :hairline="false" />
+        文本
+        <wd-divider vertical color="#1989fa" />
+        文本
+      </view>
     </demo-block>
   </page-wraper>
 </template>
 <script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content {
+  padding: 12rpx 15px;
+}
+</style>
--- src/uni_modules/wot-design-uni/components/wd-divider/wd-divider.vue
+++ src/uni_modules/wot-design-uni/components/wd-divider/wd-divider.vue
@@ -1,10 +1,6 @@
 <template>
-  <view :class="`wd-divider ${customClass}`" :style="customStyle">
-    <view class="wd-divider__line" :style="color ? 'background: ' + color : ''"></view>
-    <view class="wd-divider__content" :style="color ? 'color: ' + color : ''">
-      <slot></slot>
-    </view>
-    <view class="wd-divider__line" :style="color ? 'background: ' + color : ''"></view>
+  <view :class="rootClass" :style="rootStyle">
+    <slot v-if="!vertical"></slot>
   </view>
 </template>
 <script lang="ts">
@@ -19,9 +15,36 @@ export default {
 </script>
 
 <script lang="ts" setup>
+import { computed, useSlots } from 'vue'
 import { dividerProps } from './types'
 
-defineProps(dividerProps)
+const props = defineProps(dividerProps)
+const slots = useSlots()
+
+const rootStyle = computed(() => {
+  return `--wot-divider-color:${props.color};${props.customStyle}`
+})
+
+const rootClass = computed(() => {
+  const prefixCls = 'wd-divider'
+  if (!props.vertical) {
+    return {
+      [prefixCls]: true,
+      [`${prefixCls}--center`]: slots.default,
+      [`${prefixCls}--left`]: props.contentPosition === 'left',
+      [`${prefixCls}--right`]: props.contentPosition === 'right',
+      ['is-dashed']: props.dashed,
+      ['is-hairline']: props.hairline
+    }
+  } else {
+    return {
+      [prefixCls]: true,
+      [`${prefixCls}--vertical`]: true,
+      ['is-dashed']: props.dashed,
+      ['is-hairline']: props.hairline
+    }
+  }
+})
 </script>
 
 <style lang="scss" scoped>
