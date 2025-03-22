9b75c73ddf9ea207913fb78653b1826c3d9019a5
Mjx <798751550@qq.com>
2025-03-22 14:47:02 +0800

    feat: ✨演示文案修改

--- src/pages/msg/clear.vue
+++ src/pages/msg/clear.vue
@@ -1,7 +1,7 @@
 <template>
   <page-wraper>
     <demo-block>
-      <wd-msg type="clear" title="操作错误" desc="内容详情，根据实际需要进行展示，内容为空则不展示展示" color="#F74D54">
+      <wd-msg type="clear" title="操作错误" desc="内容详情，根据实际需要展示，内容为空则不展示" color="#F74D54">
         <!--自定义内容部分-->
         <view class="mt-5"><wd-button size="medium" type="info" block>辅助操作</wd-button></view>
       </wd-msg>
--- src/pages/msg/warn.vue
+++ src/pages/msg/warn.vue
@@ -1,7 +1,7 @@
 <template>
   <page-wraper>
     <demo-block>
-      <wd-msg type="warn" title="操作警告" desc="内容详情，根据实际需要进行展示，内容为空则不展示展示" color="#F5C84C">
+      <wd-msg type="warn" title="操作警告" desc="内容详情，根据实际需要展示，内容为空则不展示" color="#F5C84C">
         <!--自定义内容部分-->
         <view class="mt-5"><wd-button size="medium" type="info" block>辅助操作</wd-button></view>
       </wd-msg>
