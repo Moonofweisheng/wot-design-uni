c211f170c5fb71bb5b13805caa06c01fb8a2666b
不如摸鱼去 <1780903673@qq.com>
2025-05-07 22:09:04 +0800

    chore: 🚀 set vite build target es2015

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: weisheng
  * @Date: 2023-03-21 22:49:24
- * @LastEditTime: 2024-09-23 13:25:53
+ * @LastEditTime: 2025-05-07 22:06:37
  * @LastEditors: weisheng
  * @Description:
- * @FilePath: \wot-design-uni\vite.config.ts
+ * @FilePath: /wot-design-uni/vite.config.ts
  * 记得注释
  */
 import { defineConfig } from 'vite'
@@ -25,6 +25,7 @@ export default defineConfig({
     // #endif
   ],
   build: {
+    target: 'es2015',
     //   关闭生成map文件 可以达到缩小打包体积
     sourcemap: false // 这个生产环境一定要关闭，不然打包的产物会很大
   }
