0d7ed8129c623c0b423fe8da6814417417ea3114
不如摸鱼去 <1780903673@qq.com>
2025-05-14 21:43:39 +0800

    fix: 🐛 修复 Slider 处理边界值异常的问题，优化样式和事件处理逻辑 (#1050)
    ✅ Closes: #1023

--- src/subPages/slider/Index.vue
+++ src/subPages/slider/Index.vue
@@ -1,10 +1,10 @@
 <!--
  * @Author: weisheng
  * @Date: 2023-10-10 17:02:32
- * @LastEditTime: 2025-03-30 21:45:09
+ * @LastEditTime: 2025-05-13 13:16:42
  * @LastEditors: weisheng
  * @Description:
- * @FilePath: /wot-design-uni/src/pages/slider/Index.vue
+ * @FilePath: /wot-design-uni/src/subPages/slider/Index.vue
  * 记得注释
 -->
 <template>
@@ -14,6 +14,7 @@
     </demo-block>
     <demo-block :title="$t('zhi-ding-xuan-ze-fan-wei')">
       <wd-slider v-model="value2" :min="-10" :max="10" />
+      <wd-slider v-model="value8" :min="160" :max="280" :step="30" />
     </demo-block>
     <demo-block :title="$t('zhi-ding-bu-chang')">
       <wd-slider v-model="value4" hide-min-max :step="10" />
@@ -34,6 +35,8 @@ import { ref } from 'vue'
 
 const value1 = ref<number>(30)
 const value2 = ref<number>(479)
+const value8 = ref<number>(185)
+
 const value4 = ref<number>(11)
 const value5 = ref<number>(70)
 const value6 = ref<number[]>([20, 40])
