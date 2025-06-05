0c7068ef19d8451d91ca0901bb1012c8d435d433
湛蓝 <doveaz66@gmail.com>
2025-06-05 17:08:59 +0800

    feat: ✨ button组件支持微信小程序的getrealtimephonenumber 事件

--- src/uni_modules/wot-design-uni/components/wd-button/types.ts
+++ src/uni_modules/wot-design-uni/components/wd-button/types.ts
@@ -20,7 +20,7 @@ export type ButtonOpenType =
   | 'getUserInfo'
   | 'contact'
   | 'getPhoneNumber'
-  | 'getrealtimephonenumber'
+  | 'getRealtimePhoneNumber'
   | 'launchApp'
   | 'openSetting'
   | 'chooseAvatar'
