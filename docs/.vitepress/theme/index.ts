3f9887c4261a658e01927c04d090209413693a67
不如摸鱼去 <1780903673@qq.com>
2025-09-03 00:03:27 +0800

    chore: 🚀 调整百度统计 PV 统计逻辑，支持统计单页面路由切换

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: weisheng
  * @Date: 2024-10-12 22:09:33
- * @LastEditTime: 2024-12-07 20:34:33
+ * @LastEditTime: 2025-09-03 00:00:30
  * @LastEditors: weisheng
  * @Description: 
  * @FilePath: /wot-design-uni/docs/.vitepress/theme/index.ts
@@ -9,6 +9,7 @@
  */
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
+import { useRouter } from 'vitepress'
 import './styles/vars.css'
 import './styles/custom.css'
 import './styles/scrollbar.scss'
@@ -22,6 +23,13 @@ import ExternalLink from './components/ExternalLink.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+// 声明百度统计全局变量
+declare global {
+  interface Window {
+    _hmt: any[]
+  }
+}
+
 
 export default {
   ...Theme,
@@ -33,9 +41,27 @@ export default {
       'nav-bar-title-after': () => h(NavBarTitleAfter),
     })
   },
-  enhanceApp({ app }) {
+  enhanceApp({ app, router }) {
     app.component('SvgImage', SvgImage)
     app.component('ExternalLink',ExternalLink)
     app.use(ElementPlus)
+    
+    // 百度统计路由监听
+    if (typeof window !== 'undefined') {
+      // 确保百度统计已加载
+      const trackPageView = (path: string) => {
+        if (window._hmt) {
+          window._hmt.push(['_trackPageview', path])
+        }
+      }
+      
+      // 监听路由变化
+      router.onAfterRouteChanged = (to) => {
+        // 延迟执行，确保页面已完全加载
+        setTimeout(() => {
+          trackPageView(to)
+        }, 100)
+      }
+    }
   },
 }
